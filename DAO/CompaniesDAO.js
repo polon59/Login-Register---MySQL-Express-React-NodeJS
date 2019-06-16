53e6560f932561fe2e822542fc83f21601ed6115
polon59 <przemyslaw.polczak@gmail.com>
2019-06-16 23:30:42 +0200

    Implemnt method 'addNewCompany' in CompaniesDAO.js

--- DAO/CompaniesDAO.js
+++ DAO/CompaniesDAO.js
@@ -5,6 +5,22 @@ class CompaniesDAO {
         this.connection = connection;
     }
 
+    addNewCompany(request){
+        return new Promise((resolve,reject)=>{
+            const {login,password,name} = request.body;
+            console.log(login);
+            console.log(password);
+            console.log(name);
+            const sql = `INSERT INTO companies (login, password, companyName)
+            VALUES ('${login}', '${password}', '${name}');`
+
+            this.connection.query(sql, (err, result)=>{  
+                if(err)return reject(err);
+                resolve(result);
+            });
+        })
+    }
+
 }
 
 module.exports = CompaniesDAO;
