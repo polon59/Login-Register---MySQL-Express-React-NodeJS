8e07fcb8e4c7fdf54ac2c942ea886b7d126da0a0
polon59 <przemyslaw.polczak@gmail.com>
2019-06-16 23:28:09 +0200

    Add method 'setRouteCreatingCompany'

--- Controller/CompaniesController.js
+++ Controller/CompaniesController.js
@@ -1,14 +1,36 @@
+const CompaniesDAO = require('../DAO/CompaniesDAO');
 
 class CompaniesController {
     
     constructor(application, connection){
+        this.companiesDAO = new CompaniesDAO(connection);
         this.application = application;
-        this.connection = connection;
     }
-
 
     setRoutes(){
         console.log("CompaniesController setroutes");
+        this.setMainRoute();
+        this.setRouteCreatingCompany();
+    }
+
+    setMainRoute(){
+        this.application.get('/', (req,res) =>{
+            console.log("main")
+            res.send('sdfsd');
+        });
+    }
+
+    setRouteCreatingCompany(){
+        this.application.put('/addCompany', (req,res) =>{
+            this.companiesDAO.addNewCompany(req)
+            .then(()=>{
+                res.send("created company");
+            })
+            .catch(err => {
+                console.log(err.message);
+                res.send(err.message);
+            });
+        });
     }
 }
 
