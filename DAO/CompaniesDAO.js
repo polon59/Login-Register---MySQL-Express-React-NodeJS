ce15c058ce84cb4e9d01a9cdf55cb9efc1e1e383
polon59 <przemyslaw.polczak@gmail.com>
2019-06-16 23:55:51 +0200

    Implement method 'getAllCompanies'

--- DAO/CompaniesDAO.js
+++ DAO/CompaniesDAO.js
@@ -18,6 +18,17 @@ class CompaniesDAO {
         })
     }
 
+    getAllCompanies(){
+        return new Promise((resolve,reject)=>{
+            const sql = `SELECT * FROM companies;`;
+
+            this.connection.query(sql, (err, result)=>{  
+                if(err)return reject(err);
+                resolve(result);
+            });
+        })
+    }
+
 }
 
 module.exports = CompaniesDAO;
