7f3ecdfe52a10b61532c5e17ee250e5a3a7f80a1
polon59 <przemyslaw.polczak@gmail.com>
2019-06-16 19:40:57 +0200

    Implement class 'DBConnection.js

---
+++ DAO/DB init/DBConnection.js
@@ -0,0 +1,40 @@
+class DbConnection{
+    
+      constructor(){
+        this.mysql = require('mysql');
+        this.host = "localhost";
+        this.user = "root";
+        this.password = "mysQLpassword";
+        this.database = "mydb";
+      }
+    
+      createDBConnection(){
+        const {host,user,password,database} = this;
+        let connection = this.mysql.createConnection({
+          host: host,
+          user: user,
+          password: password,
+          database: database
+        });
+        return this.isConnectionSuccessful(connection);
+      }
+  
+  
+      isConnectionSuccessful(connection){
+        return new Promise((resolve,reject)=>{
+          connection.connect((error)=>{
+            if (error){
+              console.log(`Error (CHECK CONNECTION) ${error.message}`)
+              resolve(null);
+            }else{
+              console.log("Connection with database established");
+              resolve(connection);
+            }
+          })
+        })
+      }
+    
+    }
+    
+    module.exports = DbConnection;
+  
