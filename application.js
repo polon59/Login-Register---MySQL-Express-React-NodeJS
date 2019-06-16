d488857d0f59b8e7a91bc3b26b717fd313ac4c91
polon59 <przemyslaw.polczak@gmail.com>
2019-06-16 20:09:44 +0200

    Implement launch script - 'application.js'

---
+++ application.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const CompaniesController = require('./Controller/CompaniesController');
+const UserController = require('./Controller/UserController');
+const DBConnection = require('./DAO/DB init/DBConnection');
+
+const application = express();
+const PORT = process.env.PORT || 6000;
+
+const dBConnection = new DBConnection();
+const userController = new UserController(application);
+const companiesController = new CompaniesController(application);
+
+dBConnection.createDBConnection().then(connection =>{
+    if (!connection) process.exit();
+    userController.setRoutes();
+    companiesController.setRoutes();
+    app.listen(port, () => console.log(`Server listening on port ${port}`));
+})
+
