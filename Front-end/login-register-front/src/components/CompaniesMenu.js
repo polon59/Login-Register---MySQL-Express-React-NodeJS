157feff0814be067b33a6fdc0c7ccb3065a2c7f9
polon59 <przemyslaw.polczak@gmail.com>
2019-06-17 02:23:57 +0200

    Add fetch method error handling in 'CompaniesMenu.js' component

--- Front-end/login-register-front/src/components/CompaniesMenu.js
+++ Front-end/login-register-front/src/components/CompaniesMenu.js
@@ -5,12 +5,60 @@ class CompaniesMenu extends React.Component{
 
     constructor(props){
         super();
+        this.state = {
+            companies : null
+        }
+    }
+
+    fetchCompaniesData(){
+        return fetch('http://localhost:8000/registeredCompanies',{
+            method: "GET",
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            }
+          })
+          .then(response => response.json())
+          .then(data => {return data;})
+    }
+
+    componentWillMount(){
+        this.fetchCompaniesData()
+        .then(result =>{
+            this.setState({
+                companies : result
+            });
+        })
+        .catch((err)=>{
+            alert("intercepted")
+            this.setState({
+                companies : "ERR"
+            });
+        })
     }
 
     render(){
+        const {companies} = this.state;
+        if (!companies){
+            return(
+                <div>
+                    <h1>companies Menu</h1>
+                    <p>Loading...</p>
+                </div>
+            )
+        } else if(companies === "ERR"){
+            return(
+                <div>
+                    <h1>companies Menu</h1>
+                    <p>Request error. Try again later.</p>
+                </div>
+            )
+        }
+
         return(
             <div>
                 <h1>companies Menu</h1>
+                {JSON.stringify(companies)}
             </div>
         )
     }
