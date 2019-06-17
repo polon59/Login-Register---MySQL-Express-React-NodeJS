2a5575a6c6205d896832eed05b713ee5b868cbfe
polon59 <przemyslaw.polczak@gmail.com>
2019-06-17 11:48:57 +0200

    Implement method 'sendNewCompanyData'

--- Front-end/login-register-front/src/Fetchers/CompanyFetcher.js
+++ Front-end/login-register-front/src/Fetchers/CompanyFetcher.js
@@ -12,6 +12,29 @@ class CompanyFetcher{
           .then(response => response.json())
           .then(data => {return data;})
     }
+
+
+    sendNewCompanyData(data){
+        return fetch('http://localhost:8000/addCompany', {
+            method: "PUT",
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: data
+          })
+          .then((res)=>{
+              console.log(res.status + "STATUS")
+
+            if (data === 'created company') return "OK";
+            else if (data === 'loginExists') return 'badLogin';
+            return "ERR"
+          })
+          .catch(error => {
+            console.log(error);
+            return "ERROR";
+          });
+    }
 }
 
 export default CompanyFetcher;
