b9a72e9c2c53b3d822b5dbe13b1bd0d6d9a97c7c
polon59 <przemyslaw.polczak@gmail.com>
2019-06-21 02:24:32 +0200

    Implement method handleLoginStatus

--- Front-end/login-register-front/src/components/Login/LoginPage.js
+++ Front-end/login-register-front/src/components/Login/LoginPage.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import {withRouter} from 'react-router-dom';
 
 
 class LoginPage extends Component{
 
     constructor(props){
         super();
+        this.props = props;
         this.companyFetcher = props.companyFetcher;
+        this.state = {
+            authenticated : null
+        }
     }
 
     handleSubmit = (e) =>{
@@ -14,10 +19,34 @@ class LoginPage extends Component{
         this.companyFetcher.sendLoginData(this.stringifyFormData(data))
         .then(result=>{
             alert(result);
+            this.handleLoginStatus(result);
         })
         .catch(err=>{
             console.log(err)
         })
+    }
+
+    handleLoginStatus = (status) =>{
+        const {history} = this.props;
+        switch (status) {
+            case 200:
+                alert("OK")
+                this.props.authenticate();
+                history.push('/protected');
+                break;
+            case 401:
+                alert("AUTH FAIL")
+                this.setState({
+                    authenticated : false
+                })
+                break;
+            default:
+                alert("DEFAULT")
+                this.setState({
+                    authenticated : "ERR"
+                })
+                break;
+        }
     }
 
     stringifyFormData = (fd) => {
@@ -29,22 +58,25 @@ class LoginPage extends Component{
       }
 
     render(){
-        return(
-           <div>
-            <h1>Login Page</h1>
-            <form onSubmit={this.handleSubmit}>
-                <label htmlFor="login">Enter login</label>
-                <input id="login" name="login" type="text" />
-                <br/><br/>
-                <label htmlFor="password">Enter your password</label>
-                <input id="password" name="password" type="password" />
-                <br/><br/>
-                <button>Send data!</button>
-            </form>
-            <br/>
-        </div>
-        )
+        if (!this.state.authenticated){
+            return(
+                <div>
+                 <h1>Login Page</h1>
+                 <form onSubmit={this.handleSubmit}>
+                     <label htmlFor="login">Enter login</label>
+                     <input id="login" name="login" type="text" />
+                     <br/><br/>
+                     <label htmlFor="password">Enter your password</label>
+                     <input id="password" name="password" type="password" />
+                     <br/><br/>
+                     <button>Send data!</button>
+                 </form>
+                 <br/>
+             </div>
+             )
+        }
+        
     }
 }
 
-export default LoginPage;
+export default withRouter(LoginPage);
