8c5d1014ad3a73c92cc9b57c13e81cb85e75f3da
polon59 <przemyslaw.polczak@gmail.com>
2019-06-21 02:14:40 +0200

    Try to set cookie with token in 'setRouteLogin' method in 'LoginController.js'

--- Controller/LoginController.js
+++ Controller/LoginController.js
@@ -1,4 +1,5 @@
 const CompaniesDAO = require('../DAO/CompaniesDAO');
+const jwt = require('jsonwebtoken');
 
 class LoginController {
     
@@ -8,24 +9,30 @@ class LoginController {
     }
 
     setRoutes(){
-        console.log("LoginController setroutes");
         this.setRouteLogin();
     }
 
     setRouteLogin(){
-        this.application.post('/login', (req,res) =>{
-            this.companiesDAO.checkLoginParameters(req)
+        this.application.post('/login', (request,res) =>{
+            this.companiesDAO.checkLoginParameters(request)
             .then((result)=>{
                 if (result.length < 1) {
                     res.status(401).send();
                     console.log("authentication failed");
                     return;
                 }
-                req.session.loggedin = true;
-                req.session.userId = result[0].id;
-                console.log(`LOGGED USER ID=${result[0].id}`);
-                res.status(200).send({userID : result[0].id});
-            })
+
+                const secret = 'secret';
+                const userId = result[0].id;
+
+                const token = jwt.sign({
+                    data: userId
+                  }, secret, { expiresIn: '1h' });
+
+                res.cookie('token', token, { httpOnly: true })
+                .sendStatus(200);
+              }
+            )
             .catch(err => {
                 console.log(err);
                 res.status(500).send();
